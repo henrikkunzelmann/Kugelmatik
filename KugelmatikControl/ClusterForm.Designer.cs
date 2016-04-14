c4517256116c129e6fc07aa8bc32ad2e5c09326e
henrik1235 <henrik1235@users.noreply.github.com>
2016-04-15 00:16:06 +0200

    ClusterForm größer machen

--- KugelmatikControl/ClusterForm.Designer.cs
+++ KugelmatikControl/ClusterForm.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(752, 476);
+            this.ClientSize = new System.Drawing.Size(793, 565);
             this.Name = "ClusterForm";
             this.Text = "ClusterForm";
             this.ResumeLayout(false);
